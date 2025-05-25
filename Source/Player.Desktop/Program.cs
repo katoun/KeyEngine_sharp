5953018e69c1fc6390ed1060975e438f8a7d40ed
katoun <katoun9@gmail.com>
2025-05-25 22:39:47 +0300

    wip

--- Source/Player.Desktop/Program.cs
+++ Source/Player.Desktop/Program.cs
@@ -1,48 +1,70 @@
 ﻿using Silk.NET.Input;
 using Silk.NET.Maths;
+using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
+using System;
+using System.Drawing;
 
 namespace KeyEngine.Player.Desktop;
 
 internal class Program
 {
     private static IWindow m_Window;
+    private static GL m_Gl;
 
     public static void Main(string[] args)
     {
         WindowOptions options = WindowOptions.Default;
         options.Size = new Vector2D<int>(800, 600);
-        options.Title = "My first Silk.NET program!";
+        options.Title = "Player.Desktop w/ Silk.NET";
 
         m_Window = Window.Create(options);
 
         m_Window.Load += OnLoad;
-        m_Window.Update += OnUpdate;
         m_Window.Render += OnRender;
+        m_Window.Update += OnUpdate;
+        m_Window.Closing += OnClose;
+        m_Window.FramebufferResize += OnFramebufferResize;
+        
 
         m_Window.Run();
+
+        m_Window.Dispose();
     }
 
-    private static void OnLoad()
+    private static unsafe void OnLoad()
     {
-        Console.WriteLine("Load!");
-
         IInputContext input = m_Window.CreateInput();
         for (int i = 0; i < input.Keyboards.Count; i++)
         {
             input.Keyboards[i].KeyDown += KeyDown;
         }
+
+        //Getting the opengl api for drawing to the screen.
+        m_Gl = m_Window.CreateOpenGL();
+
+        m_Gl.ClearColor(Color.CornflowerBlue);
     }
 
-    // These two methods are unused for this tutorial, aside from the logging we added earlier.
+    private static unsafe void OnRender(double deltaTime)
+    {
+        //Clear the color channel.
+        m_Gl.Clear(ClearBufferMask.ColorBufferBit);
+    }
+
     private static void OnUpdate(double deltaTime)
     {
-        Console.WriteLine("Update!");
+       
     }
 
-    private static void OnRender(double deltaTime)
+    private static void OnFramebufferResize(Vector2D<int> newSize)
     {
-        Console.WriteLine("Render!");
+        m_Gl.Viewport(newSize);
+    }
+
+    private static void OnClose()
+    {
+        //Remember to delete the buffers.
     }
 
     private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
